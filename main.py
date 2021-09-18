b28e949cab21a27cf92e6b8d303041fea98143ed
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2021-09-17 22:25:43 -0300

    Curso "String em Python: Extraindo informações de uma URL" Aula 02 Tópico 02

--- main.py
+++ main.py
@@ -14,8 +14,12 @@ print(f'QUERY: {parametros}')
 url = "bytebank.com/cambio?moedaOrigem=real"
 print(url)
 
-url_base = url[0:19]
+indice_interrogacao = url.find('?')
+
+# quando o primeiro argumento do slice não é passado, o python entende que deve começar na primeira posição
+url_base = url[:indice_interrogacao]
 print(url_base)  # Vai imprimir “bytebank.com/cambio”
 
-url_parametros = url[20:36]
+# quando o segundo argumento do slice não é passado, o python entende que pegar até o fim da string
+url_parametros = url[indice_interrogacao+1:]
 print(url_parametros)
