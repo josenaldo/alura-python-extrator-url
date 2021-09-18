83b91fe6770d3a29fb57a5fb64f030b2238988cb
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2021-09-17 22:19:50 -0300

    Curso "String em Python: Extraindo informações de uma URL" Aula 01

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
+#  Minha implementação
 url = 'https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100'
 print(url)
+
+question_index = url.index('?')
+
+base = url[0:question_index]
+parametros = url[question_index+1:len(url)]
+
+print(f'PATH: {base}')
+print(f'QUERY: {parametros}')
+
+# Implementação inicial da Alura
+url = "bytebank.com/cambio?moedaOrigem=real"
+print(url)
+
+url_base = url[0:19]
+print(url_base)  # Vai imprimir “bytebank.com/cambio”
+
+url_parametros = url[20:36]
+print(url_parametros)
