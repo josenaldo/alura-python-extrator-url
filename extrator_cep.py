793a06794043b96c2a19f7dd2d86b268b11e93a4
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2021-09-20 16:14:48 -0300

    Curso "String em Python: Extraindo informações de uma URL" Aula 04 Tópico 03
    Uso de quantificadores

--- extrator_cep.py
+++ extrator_cep.py
@@ -3,7 +3,7 @@ endereco = 'Rua das Flores, 72, apartamento 1002, Laranjeiras, Rio de Janeiro, R
 import re
 
 # 5 dígitos + hífen + 3 dígitos
-padrao = re.compile('[0123456789][0123456789][0123456789][0123456789][0123456789][-][0123456789][0123456789][0123456789]')
+padrao = re.compile('[0123456789]{5}[-]{0,1}[0123456789]{3}')
 busca = padrao.search(endereco)
 
 if busca:
